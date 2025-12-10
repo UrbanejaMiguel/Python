79c46b2230edce252cdf4d04221ba5fe45291d26
Miguel Urbaneja <miguel.urbanejafernandez@milliman.com>
2025-12-10 13:36:09 +0100

    Ej_2 completado por mi

--- Ejercicios/Ej_2/lib/poliza.py
+++ Ejercicios/Ej_2/lib/poliza.py
@@ -1,4 +1,5 @@
 import pandas as pd # type: ignore
+import numpy as np # type: ignore
 
 class Poliza:
     cartera = "df"
@@ -12,5 +13,28 @@ class Poliza:
         else:
             self.cartera = pd.read_excel(self.fichero)
     
-    def med(self,a,p):
-       self.media = a[p].mean()
+    def promedio(self,c):
+       self.media = self.cartera[c].mean() 
+       return self.media
+    
+    def total_SA(self,c):
+        self.total= self.cartera[c].sum()
+        return self.total
+    
+    def df(self):
+        edad=int(input("Edad de filtro: "))
+        df_edad = self.cartera[self.cartera["edad"]>edad]
+        return df_edad
+
+    def va(self):
+        años= int(input("¿Cuántos años quieres proyectar? "))
+        i = float(input("¿Cúal es el tipo de interés? "))
+        spot= (1+i)**np.arange(años)
+        primas_va= self.cartera["prima_anual"]
+        primas_va = primas_va.to_numpy()
+        primas_va = primas_va.reshape(-1,1)
+        primas_va=np.tile(primas_va,años)
+        primas_va=pd.DataFrame(primas_va)
+        primas_va= primas_va/spot
+        primas_va=primas_va.sum(axis=1)
+        return primas_va
--- Ejercicios/Ej_2/main.py
+++ Ejercicios/Ej_2/main.py
@@ -8,23 +8,37 @@ def main():
 [1]. Calcular prima media
 [2]. Calcular suma asegurada total
 [3]. Calcular edad media
+[4]. Crear dataset filtrado
+[5]. Calcular VA de primas
 [x]. Salir de la app
 """
     print(menu)
     option =input("Selecciona una opción: ")
     if option == "1":
         cartera = Poliza("cartera_polizas","csv")
+        prima_media= cartera.promedio("prima_anual")
+        print(f"La prima anual media de la cartera es {prima_media}") 
     elif option=="2":
         cartera = Poliza("cartera_polizas","csv")
+        total_sa= cartera.total_SA("suma_asegurada")
+        print(f"La suma asegurada total de la cartera es {total_sa:,}") 
     elif option == "3":
         cartera = Poliza("cartera_polizas","csv")
+        edad_media= cartera.promedio("edad")
+        print(f"La edad media de la cartera es {edad_media}") 
+    elif option == "4":
+        cartera = Poliza("cartera_polizas","csv")
+        df=cartera.df()
+        print(f"La nueva cartera es: \n {df}") 
+    elif option == "5":
+        cartera = Poliza("cartera_polizas","csv")
+        va=cartera.va()
+        print(f"El valor actual de las primas es \n {va}") 
     elif option =="x":
         print("Hasta pronto")
     else:
         print("Opcion no valida")
         main()
-
-
 
 if __name__ == "__main__":
     main()
